f9d9b26ced9444fb5eeb24aad2919fefb0a358fb
bhoomi gaheja <bhoomigaheja.ps12@gmail.com>
2024-05-19 21:00:04 +0530

    first commit

--- commerce/settings.py
+++ commerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["auction-49iu.onrender.com"]
 
 
 # Application definition
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "default": dj_database_url.parse('postgres://root_kao9_user:oGzcELSBH9SfhJDXIz71a0jaFNUwgsn3@dpg-cp4oj88cmk4c73emvetg-a.singapore-postgres.render.com/root_kao9')
     }
-}
+# }
 
 AUTH_USER_MODEL = 'auctions.User'
 
@@ -120,7 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Configure Django App for Heroku.
 import django_heroku
